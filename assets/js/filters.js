f4ceab6e614d01239f20590ed6ef36e4d341acc3
Chris Maj <c.maj@cqu.edu.au>
2014-11-19 15:08:47 +1000

    Reformat summary table and add boolean UI filter

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -7,4 +7,21 @@ angular.module('redboxAdmin.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     };
+  }])
+  .filter('prettyBool', [function(){
+	  return function(bool){
+		  var aff = ['true', '1'];
+		  var neg = ['false', '0'];
+		  
+		  if(aff.indexOf(bool.toString().toLowerCase()) >= 0){
+			  return 'Yes';
+		  }else if(neg.indexOf(bool.toString().toLowerCase()) >=0){
+			  return 'No';
+		  }else{
+			  return bool;
+		  }
+		  
+		  
+		  
+	  };
   }]);
