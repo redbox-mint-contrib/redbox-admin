7bf741727c7bd68a1e3c6a6f8052eaf303074a2e
ubuntu <you@example.com>
2014-11-26 01:04:20 +0000

    Tidy up builder's stage controller on server

--- api/controllers/FormBuilderController.js
+++ api/controllers/FormBuilderController.js
@@ -6,121 +6,81 @@
  */
 
 module.exports = {
-  formConfsPath : "/opt/redbox/home/form-configuration/",
+  formConfsPath : sails.config.instance['redbox'].installPath + "home/form-configuration/",
+  /**
+   * `List conf files from formConfsPath`
+   */
+  get: function (req, res) {
+    var fl = [];
+    if (module.exports.formConfsPath) {
+      var fs = require('fs');
+      fl = fs.readdirSync(module.exports.formConfsPath);
+    } else {
+      sails.log.error("No path is specified.");
+    };
+    res.json({ flist: fl });
+  },
   getStageList:function (req, res) {
     var confName = req.param("fileName");
     if (confName) {
-        var fs = require('fs');
-        //~ confName = "/opt/redbox/home/form-configuration/arms_form.json";
-        //~ var obj = JSON.parse(fs.readFileSync("/opt/redbox/home/form-configuration/" + confName));
-        console.log(module.exports.formConfsPath);
-        var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
-        var stageArray = [];
-        for(stage in obj.stages) {
-            stageArray.push(stage);
-        }
-        //res.send(obj);
-        stageArray.sort();
-        res.json({
-                stages: stageArray
-        });
-    } else {
-        res.json({
-                    name: req.param("fileName")
-                });
-            }
-    },
-  /**
-   * `FormBuilderController.get()`
-   */
-  get: function (req, res) {
-    // TODO: configure where to find the installation of ReDBox
-    sails.log.info("Fixed path is used: " + module.exports.formConfsPath);
-
-    var fileName = req.param('formConf');
-    if (fileName) {
-        sails.log.debug("Scan stage names from file: " + fileName);
-    } else {
-        sails.log.debug("Default behaviour: load form list");
-        var fl = null;
-        if (module.exports.formConfsPath) {
-          var fs = require('fs');
-            fl = fs.readdirSync(module.exports.formConfsPath);
-        } else {
-          sails.log.debug("No path is specified.");
-        };
-        res.json({
-                flist: fl
-            });
+      var fs = require('fs');
+      var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
+      var stageArray = [];
+      for(stage in obj.stages) {
+        stageArray.push(stage);
+      }
+      res.json({stages: stageArray});
     }
   },
   addStage:function (req, res) {
     var confName = req.param("fileName");
     var stageName = req.param("stage");
-    //~ console.log(confName);
-    //~ console.log(stageName);
 
     if (confName) {
-        var fs = require('fs');
-        console.log(module.exports.formConfsPath);
-        var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
-        if (!(stageName in obj.stages)) {
-            obj.stages[stageName] = {};
-        }
-        var stageArray = [];
-        for(stage in obj.stages) {
-            stageArray.push(stage);
-        }
-        stageArray.push("File is not saved");
-        stageArray.sort();
-        module.exports.saveConf(confName, obj);
-        res.json({
-                stages: stageArray
-        });
+      var fs = require('fs');
+      var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
+      if (!(stageName in obj.stages)) {
+        obj.stages[stageName] = {};
+      }
+      var stageArray = [];
+      for(stage in obj.stages) {
+        stageArray.push(stage);
+      }
+      module.exports.saveConf(confName, obj);
+      res.json({ stages: stageArray });
     } else {
-        res.json({
-                    stages: []
-                });
-            }
-    },
+      res.json({ stages: [] });
+    }
+  },
   removeStage:function (req, res) {
     var confName = req.param("fileName");
     var stageName = req.param("stage");
-    //~ console.log(confName);
-    //~ console.log(stageName);
 
     if (confName) {
-        var fs = require('fs');
-        console.log(module.exports.formConfsPath);
-        var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
-        if (stageName in obj.stages) {
-            console.log("Find " + stageName + ", going to remove it");
-            delete obj.stages[stageName];
-        }
-        var stageArray = [];
-        for(stage in obj.stages) {
-            stageArray.push(stage);
-        }
-        stageArray.push("File is not saved");
-        stageArray.sort();
-        module.exports.saveConf(confName, obj);
-        res.json({
-                stages: stageArray
-        });
-    } else {
-        res.json({
-                    stages: []
-                });
-            }
-    },
-  saveConf:function(confName, newConf) {
       var fs = require('fs');
-      try {
-        console.warn("File path is not used, so it is saved to redbox-admin/");
-        fs.writeFileSync(confName, JSON.stringify(newConf));
-      } catch (e) {
-        console.log("File: " + confName + " has not been updated. Reason: " + e);
+      var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
+      if (stageName in obj.stages) {
+        delete obj.stages[stageName];
       }
+      var stageArray = [];
+      for(stage in obj.stages) {
+        stageArray.push(stage);
+      }
+      module.exports.saveConf(confName, obj);
+      res.json({ stages: stageArray });
+    } else {
+        res.json({ stages: [] });
+    }
+  },
+  saveConf:function(confName, newConf) {
+    confName = module.exports.formConfsPath + 't' + confName;
+    console.warn("File is saved to a new name for testing:" + confName);
+    var fs = require('fs');
+    try {
+      fs.writeFileSync(confName, JSON.stringify(newConf));
+    } catch (e) {
+      console.log("File: " + confName + " has not been updated. Reason: " + e);
+    }
   }
 };
 
