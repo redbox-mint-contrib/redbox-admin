7f472ac4ba9128a36ab3f2c1d9970c464b04a04f
Jianfeng Li <li@redboxresearchdata.com.au>
2015-01-06 17:08:55 +1030

    Add FormLoader factory function

--- assets/js/workflow-controllers.js
+++ assets/js/workflow-controllers.js
@@ -46,11 +46,10 @@ angular.module('redboxAdmin.controllers').controller('WorkflowsCtrl', ['$scope',
     });
   };
 }])
-.controller('StageSecsCtrl', ['$scope', '$resource', '$routeParams', 'Workflow', 'ConfSaver', function ($scope, $resource, $routeParams, Workflow, ConfSaver) {
-  var conf = $routeParams.formConf;
-  var stage = $routeParams.stage;
-  $scope.stage = stage;
+.controller('StageSecsCtrl', ['$scope', '$resource', '$routeParams', 'Workflow', 'ConfSaver', 'FormLoader', function ($scope, $resource, $routeParams, Workflow, ConfSaver, FormLoader) {
+  var params = {formConf: $routeParams.formConf, stage: $routeParams.stage};
 
+  $scope.loaded = false;
   // Default form looking
   $scope.form = [
     {
@@ -90,125 +89,41 @@ angular.module('redboxAdmin.controllers').controller('WorkflowsCtrl', ['$scope',
     {type:"submit", title: "Save"}
   ];
 
-  var params = {formConf: conf, stage: stage};
+  FormLoader(params, $scope);
 
-  $scope.loaded = false;
-
-  var supportedComponents; //Used for conditional display
-  function prepareCTypes(field) {
-    // prepare configs for component types in form, called when form is being bulit
-    for (var i=0, l=supportedComponents.length; i < l; i++) {
-      field.push({"type":"conditional",
-                  "condition":"getComponentType(model,arrayIndex) == '" + supportedComponents[i] + "'",
-                  "items":["divs[][fields][][component-confs][" + supportedComponents[i] + "]"]
-                 });
-    }
-    return field;
+  $scope.onSubmit = function(form) {
+    ConfSaver(form, $scope);
   }
-
-  Workflow.get(params, function (formDetails) {
-    $scope.formConf = params.formConf;
-    $scope.stage = params.stage;
-    $scope.schema = formDetails.schema;
-    var model = formDetails.model;
-    // transformModelForForm
-    for(var divIndex=0; divIndex < model.divs.length; divIndex++) {
-      var div = model.divs[divIndex];
-      for(var fieldIndex = 0; fieldIndex < div.fields.length; fieldIndex++) {
-        var field = div.fields[fieldIndex];
-        var componentType = field['component-type'];
-        var componentTypeProperties = {};
-        for(var fieldKey in field) {
-          if(fieldKey != 'component-type' && fieldKey !='field-name') {
-            componentTypeProperties[fieldKey] = field[fieldKey];
-            delete model.divs[divIndex]['fields'][fieldIndex][fieldKey];
-          }
-        }
-        field['component-confs'] = {};
-        field['component-confs'][componentType] = componentTypeProperties;
-        model.divs[divIndex].fields[fieldIndex] = field;
-      }
-
-    }
-    $scope.model = model;
-
-    supportedComponents = formDetails.supportedComponents;
-    $scope.form[0]['items'][1]['items'] = prepareCTypes($scope.form[0]['items'][1]['items']);
-    $scope.loaded = true;
-  });
 
   var regActiveTabIndexIndex = /.+(\d)/;
   $scope.divIndex = 0;
   $scope.getComponentType = function(model, arrayIndex) {
     //Get the current component type for conditonal display
+    // When form has more then one dimensional data, we have to hack to find out activeTab's index
+    // as arrayIndex only works for one tab
     var activeTab = $('li.ng-scope.active')[0].innerHTML;
     try {
       $scope.divIndex = regActiveTabIndexIndex.exec(activeTab)[1];
     } catch (e) { // might be the sily updating causing problem
     }
 
-    if (typeof $scope.divIndex === 'undefined') { $scope.divIndex = 0; }
-    try {
-      if (supportedComponents.indexOf($scope.model['divs'][$scope.divIndex]['fields'][arrayIndex]['component-type']) >= 0) {
-        return $scope.model['divs'][$scope.divIndex]['fields'][arrayIndex]['component-type'];
-      } else {
-        return 'notsuppored';
-      }
-    } catch (e) {}
+    if (parseInt($scope.divIndex) >= 0)  {
+      try {
+        $scope.divIndex = parseInt($scope.divIndex);
+        if ($scope.supportedComponents.indexOf($scope.model['divs'][$scope.divIndex]['fields'][arrayIndex]['component-type']) >= 0) {
+          return $scope.model['divs'][$scope.divIndex]['fields'][arrayIndex]['component-type'];
+        } else {
+          return 'notsuppored';
+        }
+      } catch (e) { /* too many to show console.error(e) */ }
+    }
   };
-  $scope.onSubmit = function(form) {
-    ConfSaver(form, $scope);
-  }
-
-//  $scope.onSubmit = function(form) {
-//    var updateParams = {fileName: conf, stage: stage}
-//    modalDiag.showModal('confirm.html', 'static', function(choice) {
-//      if (choice == 'Yes') {
-//        var model = JSON.parse(JSON.stringify($scope.model));
-//        for(var divIndex=0; divIndex < model.divs.length; divIndex++) {
-//          var div = model.divs[divIndex];
-//          for(var fieldIndex = 0; fieldIndex < div.fields.length; fieldIndex++) {
-//            var field = div.fields[fieldIndex];
-//            var componentType = field['component-type'];
-//            if ('component-confs' in field) {
-//              var configuration = field['component-confs'][componentType];
-//              for(var fieldKey in configuration) {
-//                field[fieldKey] = configuration[fieldKey];
-//              }
-//              delete field['component-confs'];
-//            }
-//            model.divs[divIndex].fields[fieldIndex] = field;
-//          }
-//        }
-////        var stageController = $resource('/redbox-admin/formBuilder/:fileName/:stage');
-////        stageController.save({fileName: conf, stage: stage}, model, function (res) { alert("Saved successfully."); });
-//        var stageController = $resource('/redbox-admin/formBuilder/:fileName/:stage', null, {update:{method: 'PUT'}});
-//        stageController.update(updateParams, model, function (res) { alert("Saved successfully."); });
-//
-//        //  Above code does not validate form, if it is needed,
-//        // First we broadcast an event so all fields validate themselves
-////        $scope.$broadcast('schemaFormValidate');
-//////
-//////        // Then we check if the form is valid
-////        if (form.$valid) {
-//////          // ... do whatever you need to do with your data.
-////            var r = confirm("Existing form configuration will be overwritten. Do you want to continue?");
-////            if (r) {
-////                alert("The file will be saved to somewhere.")
-////        //************************************TODO: Move above save to here
-////        //************************************TODO: End of Move
-////            }
-////        }
-//      }
-//    });
-//  };
 
 }])
-.controller('StageSecsCtrlInd', ['$scope', '$resource', '$routeParams', 'Workflow', 'ConfSaver', function ($scope, $resource, $routeParams, Workflow, ConfSaver) {
-  var conf = $routeParams.formConf;
-  var stage = $routeParams.stage;
-  var section = $routeParams.section;
+.controller('StageSecsCtrlInd', ['$scope', '$resource', '$routeParams', 'Workflow', 'ConfSaver', 'FormLoader', function ($scope, $resource, $routeParams, Workflow, ConfSaver, FormLoader) {
+  var params = {formConf: $routeParams.formConf, stage: $routeParams.stage, section: $routeParams.section};
 
+  $scope.loaded = false;
   // Default form looking
   $scope.form = [
     {
@@ -224,7 +139,7 @@ angular.module('redboxAdmin.controllers').controller('WorkflowsCtrl', ['$scope',
             {
               key: "divs[][fields][][component-type]"
             },
-            // more conditions are built/appended on fly
+            // more conditions are built/appended on fly, see prepareCTypes in FormLoader
             {
               type: "conditional",
               condition: "getComponentType(model,arrayIndex) == 'notsuppored'",
@@ -242,111 +157,22 @@ angular.module('redboxAdmin.controllers').controller('WorkflowsCtrl', ['$scope',
     { type:"submit", title: "Save" }
   ];
 
-  var params;
-  if(angular.isDefined(section)) {
-    params = {formConf:conf, stage:stage, section:section};
-  } else {
-    console.error("Why are we here? ng-route has problem");
-  }
-
-  $scope.loaded = false;
-
-  var supportedComponents; //Used for conditional display
-  function prepareCTypes(field) {
-    // prepare configs for component types in form, called when form is being bulit
-    for (var i=0, l=supportedComponents.length; i < l; i++) {
-      field.push({"type":"conditional",
-                  "condition":"getComponentType(model,arrayIndex) == '" + supportedComponents[i] + "'",
-                  "items":["divs[][fields][][component-confs][" + supportedComponents[i] + "]"]
-                 });
-    }
-    return field;
-  }
-
-  Workflow.get(params, function (formDetails) {
-    $scope.formConf = params.formConf;
-    $scope.stage = params.stage;
-    $scope.section = params.section;
-    $scope.schema = formDetails.schema;
-    var model = formDetails.model;
-    // transformModelForForm
-    for(var divIndex=0; divIndex < model.divs.length; divIndex++) {
-      var div = model.divs[divIndex];
-      for(var fieldIndex = 0; fieldIndex < div.fields.length; fieldIndex++) {
-        var field = div.fields[fieldIndex];
-        var componentType = field['component-type'];
-        var componentTypeProperties = {};
-        for(var fieldKey in field) {
-          if(fieldKey != 'component-type' && fieldKey !='field-name') {
-            componentTypeProperties[fieldKey] = field[fieldKey];
-            delete model.divs[divIndex]['fields'][fieldIndex][fieldKey];
-          }
-        }
-        field['component-confs'] = {};
-        field['component-confs'][componentType] = componentTypeProperties;
-        model.divs[divIndex].fields[fieldIndex] = field;
-      }
-
-    }
-    $scope.model = model;
-
-    supportedComponents = formDetails.supportedComponents;
-    $scope.form[0]['items'][1]['items'] = prepareCTypes($scope.form[0]['items'][1]['items']);
-    $scope.loaded = true;
-  });
+  FormLoader(params, $scope);
 
   $scope.onSubmit = function(form) {
     ConfSaver(form, $scope);
   }
 
-//  $scope.onSubmit = function(form) {
-//    console.log("Can we get section? " + section);
-//    var updateParams = {fileName: conf, stage: stage}
-//    if(angular.isDefined(section)) {
-//      console.log("We should update a section only");
-//      updateParams['section'] = section;
-//    }
-//    modalDiag.showModal('confirm.html', 'static', function(choice) {
-//      if (choice == 'Yes') {
-//        var model = JSON.parse(JSON.stringify($scope.model));
-//        for(var divIndex=0; divIndex < model.divs.length; divIndex++) {
-//          var div = model.divs[divIndex];
-//          for(var fieldIndex = 0; fieldIndex < div.fields.length; fieldIndex++) {
-//            var field = div.fields[fieldIndex];
-//            var componentType = field['component-type'];
-//            if ('component-confs' in field) {
-//              var configuration = field['component-confs'][componentType];
-//              for(var fieldKey in configuration) {
-//                field[fieldKey] = configuration[fieldKey];
-//              }
-//              delete field['component-confs'];
-//            }
-//            model.divs[divIndex].fields[fieldIndex] = field;
-//          }
-//        }
-////        var stageController = $resource('/redbox-admin/formBuilder/:fileName/:stage');
-////        stageController.save({fileName: conf, stage: stage}, model, function (res) { alert("Saved successfully."); });
-//        var stageController = $resource('/redbox-admin/formBuilder/:fileName/:stage', null, {update:{method: 'PUT'}});
-//        stageController.update(updateParams, model, function (res) { alert("Saved successfully."); });
-//
-//        //  Above code does not validate form, if it is needed,
-//        // First we broadcast an event so all fields validate themselves
-////        $scope.$broadcast('schemaFormValidate');
-//////
-//////        // Then we check if the form is valid
-////        if (form.$valid) {
-//////          // ... do whatever you need to do with your data.
-////            var r = confirm("Existing form configuration will be overwritten. Do you want to continue?");
-////            if (r) {
-////                alert("The file will be saved to somewhere.")
-////        //************************************TODO: Move above save to here
-////        //************************************TODO: End of Move
-////            }
-////        }
-//      }
-//    });
-//  };
-
+  $scope.divIndex = 0;
+  $scope.getComponentType = function(model, arrayIndex) {
+    try {
+      if ($scope.supportedComponents.indexOf($scope.model['divs'][$scope.divIndex]['fields'][arrayIndex]['component-type']) >= 0) {
+        return $scope.model['divs'][$scope.divIndex]['fields'][arrayIndex]['component-type'];
+      } else {
+        return 'notsuppored';
+      }
+    } catch (e) { /* too many to show console.error(e) */ }
+  };
 }]);
 
 angular.module('redboxAdmin.controllers').factory('ConfSaver', ['modalDiag', 'Workflow', function(modalDiag, Workflow) {
@@ -399,4 +225,55 @@ angular.module('redboxAdmin.controllers').factory('ConfSaver', ['modalDiag', 'Wo
     });
   };
 }
-]);
+])
+.factory('FormLoader', ['Workflow', function(Workflow) {
+  // This Service sets a few variables in caller's $scope. Good or bad?
+  var prepareCTypes = function(supportedComponents, field) {
+    // prepare configs for known component types in form, append to the items list after 'unsported'
+    // called when form is being bulit
+    // See the 'conditional' item in form defintion
+    for (var i=0, l=supportedComponents.length; i < l; i++) {
+      field.push({"type":"conditional",
+                  "condition":"getComponentType(model,arrayIndex) == '" + supportedComponents[i] + "'",
+                  "items":["divs[][fields][][component-confs][" + supportedComponents[i] + "]"]
+                 });
+    }
+    return field;
+  };
+
+  return function(params, scope) {
+    Workflow.get(params, function (formDetails) {
+      scope.formConf = params.formConf;
+      scope.stage = params.stage;
+      scope.schema = formDetails.schema;
+      if (angular.isDefined(params.section)) {
+        scope.section = params.section;
+      }
+
+      var model = formDetails.model;
+      // transformModelForForm
+      for(var divIndex=0; divIndex < model.divs.length; divIndex++) {
+        var div = model.divs[divIndex];
+        for(var fieldIndex = 0; fieldIndex < div.fields.length; fieldIndex++) {
+          var field = div.fields[fieldIndex];
+          var componentType = field['component-type'];
+          var componentTypeProperties = {};
+          for(var fieldKey in field) {
+            if(fieldKey != 'component-type' && fieldKey !='field-name') {
+              componentTypeProperties[fieldKey] = field[fieldKey];
+              delete model.divs[divIndex]['fields'][fieldIndex][fieldKey];
+            }
+          }
+          field['component-confs'] = {};
+          field['component-confs'][componentType] = componentTypeProperties;
+          model.divs[divIndex].fields[fieldIndex] = field;
+        }
+      }
+      scope.model = model;
+
+      scope.supportedComponents = formDetails.supportedComponents;
+      scope.form[0]['items'][1]['items'] = prepareCTypes(scope.supportedComponents, scope.form[0]['items'][1]['items']);
+      scope.loaded = true;
+    });
+  }
+}]);
