e964c632b4aca403cca2c626a04d399135d3e426
Jianfeng Li <li@redboxresearchdata.com.au>
2015-03-06 12:40:00 +1030

    Save nested conf file works now
    Tell client if all-in-one type mixed with nested one

--- api/controllers/FormEditorController.js
+++ api/controllers/FormEditorController.js
@@ -27,27 +27,6 @@ module.exports = {
     };
     res.json({ flist: fl });
   },
-  isFormDef: function(f) {
-    var fs = module.exports.gfs;
-    var fPath = module.exports.formConfsPath + f;
-    if (fs.statSync(fPath).isDirectory()) {
-      return false;
-    }
-    var path = require('path');
-    var bn = path.basename(f, '.json');
-    // if file name has no .json as ext or has backup at the beginning, the file is not qualified
-    if (bn == f || bn.indexOf('backup') == 0) {
-      return false;
-    }
-    var obj = {};
-    try {
-      obj = JSON.parse(fs.readFileSync(fPath));
-    } catch (e) {
-      sails.log.warn(fPath + ' was not parsed successfully by JSON, it says:');
-      sails.log.warn(e);
-    }
-    return 'stages' in obj;
-  },
   getStagesList:function (req, res) {
   /**
    * List stages of a conf file
@@ -61,46 +40,6 @@ module.exports = {
         stageArray.push(stage);
       }
       res.json({stages: stageArray});
-    }
-  },
-  addStage:function (req, res) {
-    var confName = req.param("fileName");
-    var stageName = req.param("stage");
-
-    if (confName) {
-      var fs = module.exports.gfs;
-      var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
-      if (!(stageName in obj.stages)) {
-        obj.stages[stageName] = {};
-      }
-      var stageArray = [];
-      for(stage in obj.stages) {
-        stageArray.push(stage);
-      }
-      EditorService.saveConf(confName, obj);
-      res.json({ stages: stageArray });
-    } else {
-      res.json({ stages: [] });
-    }
-  },
-  removeStage:function (req, res) {
-    var confName = req.param("fileName");
-    var stageName = req.param("stage");
-
-    if (confName) {
-      var fs = module.exports.gfs;
-      var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
-      if (stageName in obj.stages) {
-        delete obj.stages[stageName];
-      }
-      var stageArray = [];
-      for(stage in obj.stages) {
-        stageArray.push(stage);
-      }
-      EditorService.saveConf(confName, obj);
-      res.json({ stages: stageArray });
-    } else {
-        res.json({ stages: [] });
     }
   },
   getStage: function(req, res) {
@@ -121,7 +60,7 @@ module.exports = {
 
     if ('config-file' in model) {
       //if stages have config-file, load that file
-      model = module.exports.loadStage('../' + model['config-file']);
+      model = module.exports.readStageConf('../' + model['config-file'], true);
     }
     try {
       if (list) {
@@ -131,6 +70,7 @@ module.exports = {
           loaded.model.divs.push(model.divs[i]['heading']);
         }
       } else {
+        // stage configuration json obj and schema are needed
         var schema_path = module.exports.formConfsPath + module.exports.formSchema;
         var fs = module.exports.gfs;
         if (fs.existsSync(schema_path)) {
@@ -167,30 +107,214 @@ module.exports = {
     }
     res.send(loaded);
   },
-  // load nested files: in this file, if divs have config-file, load them
-  // Saving back will be a problem, so this is just a learning process.
-  loadStage: function(confName) {
-    console.log("Loading workflow JSON file: " + confName);
-    var stage = JSON.parse(module.exports.gfs.readFileSync(module.exports.formConfsPath + confName));
-    var divs = [], divConf = null;
-    for (var i=0; i<stage.divs.length; i++) {
-      divConf = '../' + stage.divs[i]['config-file'];
-      divs[i] = JSON.parse(module.exports.gfs.readFileSync(module.exports.formConfsPath + divConf));
+  removeStage:function (req, res) {
+    var confName = req.param("fileName");
+    var stageName = req.param("stage");
+
+    if (confName) {
+      var fs = module.exports.gfs;
+      var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
+      if (stageName in obj.stages) {
+        delete obj.stages[stageName];
+      }
+      var stageArray = [];
+      for(stage in obj.stages) {
+        stageArray.push(stage);
+      }
+      EditorService.saveConf(confName, obj);
+      res.json({ stages: stageArray });
+    } else {
+        res.json({ stages: [] });
     }
-    stage.divs = divs;
+  },
+  updateStage:function(req, res) {
+    var confName = req.param("fileName");
+    var stageName = req.param("stage");
+    var section = req.param("section"); // if section is specified
+
+    var fs = module.exports.gfs;
+    var conf = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
+    var stages_path = module.exports.isNestedConf(conf['stages']);
+    var status = { code: 200 };
+    if (Object.keys(req.body).length === 0) {
+      // add a new stage
+      if (stages_path) {
+        /* It needs to create a new empty stage file in sub-dirs like $stages_path
+           "stageName": { "config-file": "form-configuration/$stages_path/stageName.json" }
+           and tab definitions in $stages_path/tabs when details are created and then add
+           {"divs": [{'config-file': $stages_path/$divs_path/div_name.json}]}
+           There will be too many restrictions if it is done here. So do not do it.
+        */
+        status['code'] = 400;
+        status['message'] = "Cannot create new stages in a nested configuration file.";
+      } else {
+        status = module.exports.addStage(confName, stages_path, conf, stageName)
+      }
+////      console.log('Add an empty stage.');
+//      if (!(stageName in conf.stages)) {
+//        if (stages_path) {
+//          status['message'] = "Cannot create new stages in a nested configuration file.";
+////          console.log("Smart pant is needed");
+////          var json_file = stages_path + '/' + stageName + '.json';
+////          conf.stages[stageName] = {'config-file': json_file};
+////          // create a new empty stage file.
+////          // new stage file will not nest divs as I do not want to create sub-dirs like $stages_path/tabs:
+////          // {"divs": [{'config-file': $stages_path/tabs/tab_name.json}]}
+////          // so do not mix two types together
+////          EditorService.saveConf('../' + json_file, JSON.stringify({"divs": []}));
+//        } else {
+//          conf.stages[stageName] = {divs:[]};
+//        }
+//        EditorService.saveConf(confName, conf);
+//      }
+//      var stageArray = [];
+//      for(stageName in conf.stages) {
+//        stageArray.push(stageName);
+//      }
+//      status['stages'] = stageArray;
+    } else {
+      // update stage configuration json
+//      console.log('Update form definition.');
+//      console.log(JSON.stringify(req.body));
+      if (stages_path) {
+        if ('config-file' in conf['stages'][stageName]) {
+          //if stages have config-file, load that file
+          stageConf = module.exports.readStageConf('../' + conf['stages'][stageName]['config-file']);
+          var divs_path = module.exports.isNestedConf(stageConf['divs']);
+          if (divs_path) {
+          // stages are nested, make sure stage's divs are nested
+            console.log("Deeply nested as expected, smartness is needed to update " + stageName);
+            status = module.exports.updateNestedDivsConf(stageConf['divs'], req.body, section);
+            console.log(JSON.stringify(status));
+          } else {
+            status['code']= 400;
+            status['message'] = "Definition file of divs in a workflow stage has to be also in seprated files.";
+          }
+        } else {
+            status['code']= 400;
+            status['message'] = "Key config-file was not found.";
+        }
+      } else {
+        //console.log("Update all-in-one conf file");
+        if (section) {
+  //        console.log("  ----update section " + section + " of " + confName);
+  //        console.log(JSON.stringify(req.body['divs'][0]));
+          // {divs:[], form-layout: {}}
+          // so only pick up divs and there is should only one
+          conf['stages'][stage]['divs'][section] = req.body['divs'][0];
+        } else {
+            conf['stages'][stage] = req.body;
+        }
+        status = EditorService.saveConf(confName, conf);
+        //res.json(status);
+      }
+    }
+//      if (section) {
+////        console.log("  ----update section " + section + " of " + confName);
+////        console.log(JSON.stringify(req.body['divs'][0]));
+//        // {divs:[], form-layout: {}}
+//        // so only pick up divs and there is should only one
+//        conf['stages'][stage]['divs'][section] = req.body['divs'][0];
+//      } else {
+////        console.log("  ----update whole file of " + confName);
+//        conf['stages'][stage] = req.body;
+//      }
+//      status = EditorService.saveConf(confName, conf);
+//      //res.json(status);
+//    }
+    res.json(status);
+  },
+  // below are internal functions
+  isFormDef: function(f) {
+    var fs = module.exports.gfs;
+    var fPath = module.exports.formConfsPath + f;
+    if (fs.statSync(fPath).isDirectory()) {
+      return false;
+    }
+    var path = require('path');
+    var bn = path.basename(f, '.json');
+    // if file name has no .json as ext or has backup at the beginning, the file is not qualified
+    if (bn == f || bn.indexOf('backup') == 0) {
+      return false;
+    }
+    var obj = {};
+    try {
+      obj = JSON.parse(fs.readFileSync(fPath));
+    } catch (e) {
+      sails.log.warn(fPath + ' was not parsed successfully by JSON, it says:');
+      sails.log.warn(e);
+    }
+    return 'stages' in obj;
+  },
+  isNestedConf: function(confObj) {
+    /* Check if this is a nested version like used in ARMS, if yes, return path to the next level
+    * Caller has to send in an object for checking:
+    * Mostly there are only two levels:
+    * 1. top level conf['stages']
+    * 2. second level, aka stage lever, stage['divs'] - array
+    */
+    console.log('Checking if nested conf');
+    console.log(JSON.stringify(confObj));
+    if (Array.isArray(confObj)) {
+      for (var i = 0; i < confObj.length; i++) {
+        if ('config-file' in confObj[i]) {
+          console.log("array path, first hit: " + JSON.stringify(confObj[i]));
+          return require('path').dirname(confObj[i]['config-file']);
+        }
+      }
+    } else {
+      for (var k in confObj) {
+        if ('config-file' in confObj[k]) {
+          console.log("key path, first hit: " + JSON.stringify(confObj[k]));
+          return require('path').dirname(confObj[k]['config-file']);
+        }
+      }
+    }
+    return false;
+  },
+  readStageConf: function(confName, deep) {
+    /* Load a stage conf file and its nested config-files
+    *  returns a combined stage -- all its divs are inlcuded
+    */
+    console.log("Loading stage conf JSON file: " + confName);
+    var stage = JSON.parse(module.exports.gfs.readFileSync(module.exports.formConfsPath + confName));
+    if (deep) {
+      console.log("deep scan and send back merged");
+      var divs = [], divConf = null;
+      for (var i=0; i<stage.divs.length; i++) {
+        divConf = '../' + stage.divs[i]['config-file'];
+        divs[i] = JSON.parse(module.exports.gfs.readFileSync(module.exports.formConfsPath + divConf));
+      }
+      stage.divs = divs;
+    }
     return stage;
   },
-  extractStage: function(confName, stage) {
+  extractStage: function(confName, stageName) {
     console.log("Loading workflow JSON file: " + confName);
-    var fs = require('fs');
+    var fs = module.exports.gfs;
     var obj = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
 
-    if (stage in obj.stages) {
-        return obj.stages[stage];
+    if (stageName in obj.stages) {
+        return obj.stages[stageName];
     } else {
-        console.error("Stage " + stage + " is not in the conf file.");
+        console.error("Stage " + stageName + " is not in the conf file.");
         return {};
     }
+  },
+  addStage:function (confName, conf, stageName) {
+    // Add an empty stage into an all-in-one configruation file.
+    // Return stage names to caller
+    var status = { code: 200 };
+    if (!(stageName in conf.stages)) {
+      conf.stages[stageName] = {divs:[]};
+      EditorService.saveConf(confName, conf);
+    }
+    var stageArray = [];
+    for(stageName in conf.stages) {
+      stageArray.push(stageName);
+    }
+    status['stages'] = stageArray;
+    return status;
   },
   loadSchema: function(fName) {
     var fs = module.exports.gfs;
@@ -230,45 +354,6 @@ module.exports = {
 //      components['debug'] = {"type": "object", "properties": { content: { type: 'string'} } };
     }
     return {confs: components, types: types};
-  },
-  updateStage:function(req, res) {
-//    posted/put req.body is a stage or a section in it
-//    TODO: currently only all configurations are in the same file works
-    var confName = req.param("fileName");
-    var stage = req.param("stage");
-    var section = req.param("section"); // if section is specified
-
-    var fs = module.exports.gfs;
-    var conf = JSON.parse(fs.readFileSync(module.exports.formConfsPath + confName));
-    var status;
-    if (Object.keys(req.body).length === 0) {
-//      console.log('Add an empty stage.');
-      if (!(stage in conf.stages)) {
-        conf.stages[stage] = {};
-        EditorService.saveConf(confName, conf);
-      }
-      var stageArray = [];
-      for(stage in conf.stages) {
-        stageArray.push(stage);
-      }
-      res.json({ stages: stageArray });
-    } else {
-//      console.log('Update form definition.');
-//      console.log(JSON.stringify(req.body));
-      if (section) {
-//        console.log("  ----update section " + section + " of " + confName);
-//        console.log(JSON.stringify(req.body['divs'][0]));
-        // {divs:[], form-layout: {}}
-        // so only pick up divs and there is should only one
-        conf['stages'][stage]['divs'][section] = req.body['divs'][0];
-      } else {
-//        console.log("  ----update whole file of " + confName);
-        conf['stages'][stage] = req.body;
-      }
-//      res.json({status:200}); // for debug purpose
-      status = EditorService.saveConf(confName, conf);
-      res.json(status);
-    }
   },
   findComponents: function() {
     var fs = require('fs');
@@ -315,5 +400,16 @@ module.exports = {
     };
     components['types'].push("group");
     return components;
+  },
+  updateNestedDivsConf: function(parentNode, newContent, section) {
+    var status;
+    if (section) {
+      status = EditorService.saveConf(parentNode[section]['config-file'], newContent['divs'][0]);
+    } else {
+      for (var i = 0; i < parentNode.length; i++) {
+        status = EditorService.saveConf(parentNode[i]['config-file'], newContent['divs'][i]);
+      }
+    }
+    return status;
   }
 };
--- api/services/EditorService.js
+++ api/services/EditorService.js
@@ -6,23 +6,38 @@
  */
 
 module.exports = {
-  gfs: require('fs'),
   formConfsPath : sails.config.instance['redbox'].installPath + "home/form-configuration/",
   saveConf:function(confName, newConf) {
-    // it returns status json which can be used when client needs it
-    // or it can be safely ignored by caller
-    var backup = module.exports.formConfsPath + 'backup_' + confName;
-    confName = module.exports.formConfsPath + confName
-
-    var fs = module.exports.gfs;
-    // backup first
-    fs.renameSync(confName, backup);
+    // confName: name of configuration json. It can also have an relative path to module.exports.formConfsPath in front of file name
+    // return: status which be safely ignored by caller
 
     var status = {code:200};
+    var path = require('path');
+    var dirName = path.dirname(confName);
+    if (dirName == '.') {
+      dirName = '';
+    } else {
+      dirName = '../' + dirName + '/';
+      confName = path.basename(confName);
+    }
+    var working_dir = module.exports.formConfsPath + dirName;
+
+//    console.log('confName = ' + confName);
+//    console.log('dirname = ' + dirName);
+
+    var backup = working_dir + 'backup_' + confName;
+    confName = working_dir + confName
+//    console.log("Saving to " + confName);
+//    return status;
+
+    var fs = require('fs');
+
     try {
+      // backup first
+      fs.renameSync(confName, backup);
       fs.writeFileSync(confName, JSON.stringify(newConf));
     } catch (e) {
-      console.log("File: " + confName + " has not been updated. Reason: " + e);
+      console.error("File: " + confName + " has not been updated. Reason: " + e);
       status['code'] = 400;
       status['message'] = "Failed to save.";
     }
